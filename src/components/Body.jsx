c0a791830e450108865d059f12e44b40d1ed3e27
Allen <allencpsc@gmail.com>
2023-09-30 13:33:14 -0400

    added changes from lecture 2

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class Body extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    render(){
+        const increment = () => {
+            this.setState({count: this.state.count + 1})
+        }
+        return(
+            <React.Fragment>
+                <button type="button" className="btn btn-dark" onClick={increment}>
+                    {this.state.count}
+                </button>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Body
